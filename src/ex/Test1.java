adbe4920c4487d8df1cffb95fb3ff79b7a5ecfb1
jieonsim <161197746+jieonsim@users.noreply.github.com>
2024-03-04 22:48:34 +0900

    Add files via upload

--- src/ex/Test1.java
+++ src/ex/Test1.java
@@ -27,7 +27,7 @@ public class Test1 {
 				numbers[i][j] = cnt;
 			}
 		}
-
+		
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
 				System.out.print(numbers[i][j] + " ");
--- src/ex/Test2.java
+++ src/ex/Test2.java
@@ -1,5 +1,7 @@
 package ex;
 
+import java.util.Scanner;
+
 /*
  가위/바위/보 게임
 컴퓨터에게 랜덤하게 '가위/바위/보'중의 하나를 기억시켜놓고,
@@ -8,6 +10,31 @@ package ex;
  */
 public class Test2 {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			System.out.print("가위, 바위, 보 중에 하나를 입력하세요('그만'을 입력하면 종료됩니다) : ");
+			String userInput = sc.nextLine();
+
+			if (userInput.equals("그만")) {
+				System.out.println("프로그램 종료");
+				break;
+			}
+
+			int randomNumber = (int) (Math.random() * 3);
+			String[] rockPaperScissors = { "가위", "바위", "보" };
+			String computerChoice = rockPaperScissors[randomNumber];
+
+			if (userInput.equals(computerChoice)) {
+				System.out.println("비겼습니다.");
+			} else if ((userInput.equals("가위")) && (computerChoice.equals("보"))
+					|| (userInput.equals("바위")) && (computerChoice.equals("가위"))
+					|| (userInput.equals("보")) && (computerChoice.equals("주먹"))) {
+				System.out.println("컴퓨터 : " + computerChoice + " | 사용자 : " + userInput + "\n사용자가 이겼습니다.");
+			} else {
+				System.out.println("컴퓨터 : " + computerChoice + " | 사용자 : " + userInput + "\n컴퓨터가 이겼습니다.");
+			}
+		}
+		sc.close();
 	}
 }
